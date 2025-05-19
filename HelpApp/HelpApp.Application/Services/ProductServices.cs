5f4acd201848511cca9cc9867f774f825e32f37f
clarachence <domitilachence@gmail.com>
2025-05-19 12:42:39 -0300

    aula 1903 command de produto

--- HelpApp/HelpApp.Application/Services/ProductServices.cs
+++ HelpApp/HelpApp.Application/Services/ProductServices.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using HelpApp.Application.DTOs;
+using HelpApp.Application.Interfaces;
+using HelpApp.Domain.Interfaces;
+using HelpApp.Domain.Entities;
 
 namespace HelpApp.Application.Services
 {
-    internal class ProductServices
+    public class ProductServices : IProductService
     {
+        private IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public ProductServices(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productsEntity = await _productRepository.GetProducts();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productsEntity = await _productRepository.GetById(id);
+            return _mapper.Map<ProductDTO>(productsEntity);
+        }
+
+        public async Task Add(ProductDTO productDto)
+        {
+            var productsEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Create(productsEntity);
+        }
+
+        public async Task Update(ProductDTO productDto)
+        {
+            var productsEntity = _mapper.Map<Product>(productDto);
+            await _productRepository.Create(productsEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productsEntity = await _productRepository.GetById(id);
+            await _productRepository.Remove(productsEntity);
+        }
     }
 }

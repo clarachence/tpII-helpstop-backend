ddadd2d18b91fc0a27565dded1a4729c3f06552c
clarachence <domitilachence@gmail.com>
2025-05-26 12:45:54 -0300

    aula de commands e handlers

---
+++ HelpApp/HelpApp.Application/Products/Queries/GetProductsByIdQueries.cs
@@ -0,0 +1,14 @@
+﻿using HelpApp.Domain.Entities;
+using MediatR;
+
+namespace HelpApp.Application.Products.Queries
+{
+    public class GetProductsByIdQuery : IRequest<Product> 
+    {
+        public int Id { get; set; }
+        public GetProductsByIdQuery(int id)
+        { 
+            Id= id;
+        }   
+    }
+}

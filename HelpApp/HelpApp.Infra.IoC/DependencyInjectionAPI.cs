0e7c9e20782e1ca12f6cca0e84f79b8151047f1e
clarachence <domitilachence@gmail.com>
2025-06-09 12:43:14 -0300

    Aula0906 - Segura Roles de User

--- HelpApp/HelpApp.Infra.IoC/DependencyInjectionAPI.cs
+++ HelpApp/HelpApp.Infra.IoC/DependencyInjectionAPI.cs
@@ -1,9 +1,18 @@
 ﻿using HelpApp.Domain.Interfaces;
 using HelpApp.Infra.Data.Context;
 using HelpApp.Infra.Data.Repositories;
+using HelpApp.Application.Services;
+using HelpApp.Application.Mappings;
+using MediatR;
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using HelpApp.Application.Interfaces;
+using HelpApp.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity;
+using HelpApp.Domain.Account;
+
 
 namespace HelpApp.Infra.IoC
 {
@@ -16,9 +25,23 @@ namespace HelpApp.Infra.IoC
              options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"
             ), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<ICategoryService, CategoryServices>();
+            services.AddScoped<IProductService, ProductServices>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            var myhandlers = AppDomain.CurrentDomain.Load("HelpApp.Application");
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(myhandlers));
+
 
             return services;
         }
